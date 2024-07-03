47798efe5ce3117fd6d2938dfafd9090dab04db4
Jungea <jungea2241@gmail.com>
2024-07-04 00:00:05 +0900

    ActivityAndFragment

---
+++ app/src/main/java/com/example/musthaveexample/TwoColorActivity.kt
@@ -0,0 +1,40 @@
+package com.example.musthaveexample
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TwoColorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_two_color)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        settingButtons()
+    }
+
+    private fun settingButtons() {
+        val redButton = findViewById<Button>(R.id.button_red_fragment)
+        val blueButton = findViewById<Button>(R.id.button_blue_fragment)
+
+        redButton.setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentFrame, RedFragment())
+            fragmentTransaction.commit()
+        }
+
+        blueButton.setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragmentFrame, BlueFragment())
+            fragmentTransaction.commit()
+        }
+    }
+}
